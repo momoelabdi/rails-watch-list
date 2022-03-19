159f2f6dd0dea884fa70a0c1e82057b09ef872c0
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2022-03-19 18:40:12 +0100

    cards Style

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,16 +34,39 @@ Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal masterm
 #   end
 # end
 # puts "Movies created"
-require "json"
-require "open-uri"
-url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=07de3c127ea28e5a662af7b76f458fca'
-movies_serialized = URI.open(url).read
-movies = JSON.parse(movies_serialized)['results']
-movies.each do |movie|
-  Movie.create(
-    title: movie['original_title'],
-    overview: movie['overview'],
-    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
-    rating: movie['vote_average']
-  )
+# require "json"
+# require "open-uri"
+# url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=07de3c127ea28e5a662af7b76f458fca'
+# movies_serialized = URI.open(url).read
+# movies = JSON.parse(movies_serialized)['results']
+# movies.each do |movie|
+#   Movie.create(
+#     title: movie['original_title'],
+#     overview: movie['overview'],
+#     poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+#     rating: movie['vote_average']
+#   )
+# end
+require 'open-uri'
+require 'json'
+
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
+
+url = "http://tmdb.com/movie/top_rated"
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
 end
+puts "Movies created"
